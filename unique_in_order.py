27dd7ba3861358687671a64e43c4d1a57cfe3d80
John Kim <kim.jy.john@gmail.com>
2018-10-04 17:55:09 -0700

    Complete unique_in_order

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,13 +1,10 @@
 # unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-def not_duplicate(x,y):
-    # returns true if not duplicate
-    if x != y:
-        return True
-    return False
 
 def unique_in_order(iterable):
-    filtered = filter(lambda x, y: not_duplicate(x, y), iterable)
-    print(reduced)
+    res = []
+    for c in iterable:
+        if len(res) == 0 or res[-1] != c:
+            res.append(c)
+    return res
 
-
-unique_in_order('AAAABBBCCDAABBB')
+print(unique_in_order('AAAABBBCCDAABBB'))
